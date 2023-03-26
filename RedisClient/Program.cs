da16d1e08a8628b93bae520584c96f68952a16fb
Lior Dadon <Lior.Dadon@philips.com>
2023-03-26 08:36:48 +0300

    Add project files.

---
+++ RedisClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+
+        static async Task Main(string[] args)
+        {
+            var db = redis.GetDatabase();
+            var pong = await db.PingAsync();
+            Console.WriteLine(pong);
+        }
+    }
+}
